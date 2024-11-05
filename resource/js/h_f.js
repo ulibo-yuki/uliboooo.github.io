82fc84ccb78037799c81baf2c3815b5c2887b02c
yuki <fu08yu20ko07@icloud.com>
2024-11-05 21:22:19 +0900

    feat: can't implementation feature.

--- resource/js/h_f.js
+++ resource/js/h_f.js
@@ -1,9 +1,9 @@
 const includeHeader = new XMLHttpRequest();
-includeHeader.open("GET", "resource/html/header.html", true);
+includeHeader.open("GET", "/resource/html/header.html", true);
 includeHeader.onreadystatechange = function () {
   if (includeHeader.readyState === 4 && includeHeader.status === 200) {
     const headerHTML = includeHeader.responseText;
-    const header = document.querySelector("#header");
+    const header = document.querySelector(".header");
     header.insertAdjacentHTML("afterbegin", headerHTML);
   }
 };
