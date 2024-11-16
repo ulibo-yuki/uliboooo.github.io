1e260367873cef8572d28e0743dd1baf7f4e69a4
yuki <fu08yu20ko07@icloud.com>
2024-11-17 03:45:35 +0900

    add article

--- resource/js/h_f.js
+++ resource/js/h_f.js
@@ -3,7 +3,7 @@ includeHeader.open("GET", "resource/html/header.html", true);
 includeHeader.onreadystatechange = function () {
   if (includeHeader.readyState === 4 && includeHeader.status === 200) {
     const headerHTML = includeHeader.responseText;
-    const header = document.querySelector("#header");
+    const header = document.querySelector(".header");
     header.insertAdjacentHTML("afterbegin", headerHTML);
   }
 };
